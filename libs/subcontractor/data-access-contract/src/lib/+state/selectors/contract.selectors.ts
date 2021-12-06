5d6e5c7f89b76e38bcb9fb4ef9c43425902e093c
Chris Curnow <chris@4cast.com.au>
2021-12-06 11:33:48 +1100

    [nx-15]

--- libs/subcontractor/data-access-contract/src/lib/+state/selectors/contract.selectors.ts
+++ libs/subcontractor/data-access-contract/src/lib/+state/selectors/contract.selectors.ts
@@ -4,39 +4,73 @@ import {
   ContractState,
   contractAdapter,
 } from '../reducers/contract.reducer';
+import { selectRouteParams } from './router.selectors';
+
+// export const carsFeatureSelector = createFeatureSelector<CarState>('cars');
 
 // Lookup the 'Contract' feature state managed by NgRx
-export const getContractState =
+// RENAME 'getContractState' as 'contractsFeatureSelector'
+export const contractsFeatureSelector =
   createFeatureSelector<ContractState>(CONTRACT_FEATURE_KEY);
 
+  //  const { selectEntities, selectAll } = carAdapter.getSelectors();
 const { selectAll, selectEntities } = contractAdapter.getSelectors();
 
+
+
 export const getContractLoaded = createSelector(
-  getContractState,
+  contractsFeatureSelector,
   (state: ContractState) => state.loaded
 );
 
 export const getContractError = createSelector(
-  getContractState,
+  contractsFeatureSelector,
   (state: ContractState) => state.error
 );
 
-export const getAllContracts = createSelector(getContractState, (state: ContractState) =>
+// export const selectCars = createSelector(carsFeatureSelector, selectAll);
+// RENAME gotAllContracts as 'selectAllContracts'
+export const selectAllContracts = createSelector(contractsFeatureSelector, (state: ContractState) =>
   selectAll(state)
 );
 
-export const getContractEntities = createSelector(
-  getContractState,
+// export const selectCarEntities = createSelector(
+//   carsFeatureSelector,
+//   selectEntities
+// );
+
+
+// RENAME getContractEntities to 'selectContractEntities'
+export const selectContractEntities = createSelector(
+  contractsFeatureSelector,
   (state: ContractState) => selectEntities(state)
 );
 
 export const getSelectedId = createSelector(
-  getContractState,
+  contractsFeatureSelector,
   (state: ContractState) => state.selectedId
 );
 
+// // you can combine the `selectRouteParams` with `selectCarEntities`
+// // to get a selector for the active car for this component based
+// // on the route
+// export const selectCar = createSelector(
+//   selectCarEntities,
+//   selectRouteParams,
+//   (cars, { carId }) => cars[carId]
+// );
+
+// export const selectContract = createSelector(
+//   selectContractEntities,
+//   getSelectedId,
+//   (entities, selectedId) => (selectedId ? entities[selectedId] : undefined)
+// );
+
 export const selectContract = createSelector(
-  getContractEntities,
-  getSelectedId,
-  (entities, selectedId) => (selectedId ? entities[selectedId] : undefined)
+  selectContractEntities,
+  selectRouteParams,
+  (contracts, { id }) => contracts[id]
+  // INTERESTING, note exmaple uses cars where our original used entities. Are they the same thing?
+  // getSelectedId,
+  // (entities, selectedId) => (selectedId ? entities[selectedId] : undefined)
 );
