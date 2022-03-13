688792f7350dd1a4ac42b04e6b6b2f3cd2bbdb10
Chris Curnow <chris@4cast.com.au>
2022-03-13 18:52:15 +1100

    [NX-59] Create 'create project dates' function

--- libs/shared/data-access-models/src/lib/models/projects/project-dates.ts
+++ libs/shared/data-access-models/src/lib/models/projects/project-dates.ts
@@ -1,9 +1,20 @@
+import { setValues } from "@workspace/shared/util";
 
 export interface ProjectDates {
   adjusted?: Date;
   finish?: Date;
   forecastFinish?: Date;
   start?: Date;
+}
+
+export function createProjectDates (projectDates: ProjectDates | undefined ): ProjectDates {
+  const newProjectDates: ProjectDates = {};
+  if (projectDates) {
+    const properties = ['adjusted', 'finish', 'forecastFinish', 'start'];
+    setValues(projectDates, newProjectDates, properties);
+  }
+  return newProjectDates;
+
 }
 
 
