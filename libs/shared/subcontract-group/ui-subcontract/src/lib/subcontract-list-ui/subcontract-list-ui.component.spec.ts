31468f706ab347719aec7e56f45edf6805c805a2
Chris Curnow <chris@4cast.com.au>
2022-05-30 19:13:45 +1000

    [DV-4] Create new shared-ui component Subcontract-list-ui

---
+++ libs/shared/subcontract-group/ui-subcontract/src/lib/subcontract-list-ui/subcontract-list-ui.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SubcontractListUiComponent } from './subcontract-list-ui.component';
+
+describe('SubcontractListUiComponent', () => {
+  let component: SubcontractListUiComponent;
+  let fixture: ComponentFixture<SubcontractListUiComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SubcontractListUiComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SubcontractListUiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
