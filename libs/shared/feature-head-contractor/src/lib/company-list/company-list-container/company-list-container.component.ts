75d928185abd36a2010ec10cf98da40d1ee44bc5
Chris Curnow <chris@4cast.com.au>
2022-06-14 19:00:42 +1000

    [NX-121] Create component 'Company List Container'

---
+++ libs/shared/feature-head-contractor/src/lib/company-list/company-list-container/company-list-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fourcast-company-list-container',
+  templateUrl: './company-list-container.component.html',
+  styleUrls: ['./company-list-container.component.scss']
+})
+export class CompanyListContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
